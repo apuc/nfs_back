5b16eaf797b496c53ebd2472c64e4f88363556c6
Alexander Nikitin <nikitin-a-g@yandex.ru>
2023-12-19 02:24:56 +0700

    NFS1-46 CORS

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
